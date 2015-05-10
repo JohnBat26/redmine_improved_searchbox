c5fd014f6c03610ddd2bbfb915fcd2dadd89ad9b
GJ Roelofs <info@codepoke.net>
2015-05-10 13:30:25 +0200

    Changed CSS to conform to Modulo Theme

--- assets/javascripts/redmine_improved_searchbox.js
+++ assets/javascripts/redmine_improved_searchbox.js
@@ -1,7 +1,14 @@
 $(function(){
-	var str = $('#project_quick_jump_box option:first').text();
+
+        var str = $('#project_quick_jump_box option:first').text();
 	$('#project_quick_jump_box option:first').text("").val("");
-	$('#project_quick_jump_box').attr("data-placeholder", str).select2({ width: function() {
-		return ($('#project_quick_jump_box').width() + 32).toString() + 'px';
-	} });
+	
+	$('#project_quick_jump_box').select2({
+	    placeholder: "Select a Project",
+	    allowClear: true,
+	    width: function() {
+			return ($('#project_quick_jump_box').width() + 32).toString() + 'px';
+	}
+	});
+
 });

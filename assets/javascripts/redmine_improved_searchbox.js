1580d1221efbb266793549f46c3cb398fda48e29
GJ Roelofs <info@codepoke.net>
2015-05-10 14:29:01 +0200

    Updated to Select2 3.5.2 so that mobile is properly supported

--- assets/javascripts/redmine_improved_searchbox.js
+++ assets/javascripts/redmine_improved_searchbox.js
@@ -1,20 +1,10 @@
-$(function(){
-	if ('ontouchstart' in window || !!(navigator.msMaxTouchPoints))
-		return;
-
-        var str = $('#project_quick_jump_box option:first').text();
-	$('#project_quick_jump_box option:first').text("").val("");
+$(document).ready(function(){
 	
 	$('#project_quick_jump_box').select2({
-	    placeholder: "Select a Project",
 	    allowClear: true,
 	    width: function() {
 			return ($('#project_quick_jump_box').width() + 32).toString() + 'px';
-	}
+	    }
 	});
-
-//	$('#project_quick_jump_box').attr("data-placeholder", str).select2({ width: function() {
-//		return ($('#project_quick_jump_box').width() + 32).toString() + 'px';
-//	}});
 
 });
